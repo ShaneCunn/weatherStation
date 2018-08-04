92ddaaa88f412f9e5c8f5c812504b1396d43e329
Shane Cunningham <mayhem2277@gmail.com>
2018-08-04 17:00:51 +0100

    cleaned daily of unused code

--- resources/views/includes/daily.blade.php
+++ resources/views/includes/daily.blade.php
@@ -2,16 +2,7 @@
     <div class="weather-forecast daily">
         <div class="wf-title">Daily Forecast</div>
         <div>
-
-
             @foreach($dailyS as $key => $value)
-
-
-
-
-
-
-
                 <a href="https://meteo.test/location/2964180/day/02" class="wf-list">
                     <div class="wf-list-col wf-date">
                         <div class="wf-day">{{$value['day']}}</div>
@@ -39,112 +30,8 @@
                         </div>
                     </div>
                 </a>
-
             @endforeach
-            {{--  <a href="https://meteo.test/location/2964180/day/03" class="wf-list">
-                  <div class="wf-list-col wf-date">
-                      <div class="wf-day">Friday</div>
-                      <div class="wf-date">08/03/2018</div>
-                  </div>
-                  <div class="wf-list-col wf-conditions">
-                      <div class="wf-condition-row"><img
-                              src="{{asset('assets/images/icons/conditions/condition.svg')}}">
-                          <div class="wf-conditions-text">Conditions: light rain</div>
-                      </div>
 
-                      <div class="wf-condition-row"><img
-                              src="{{asset('assets/images/icons/conditions/humidity.svg')}}">
-                          <div class="wf-conditions-text">Humidity: 97%</div>
-                      </div>
-                  </div>
-                  <div class="wf-list-col wf-temp-icon">
-                      <div class="wf-icon"><img src="{{asset('assets/images/icons/weather/10.svg')}}" class="wf-icon">
-                      </div>
-                      <div class="wf-temp">
-                          <div class="wf-temp-max">20°</div>
-                          <div class="wf-temp-min">15°</div>
-                      </div>
-                  </div>
-              </a>
-              <a href="https://meteo.test/location/2964180/day/04" class="wf-list">
-                  <div class="wf-list-col wf-date">
-                      <div class="wf-day">Saturday</div>
-                      <div class="wf-date">08/04/2018</div>
-                  </div>
-
-                  <div class="wf-list-col wf-conditions">
-                      <div class="wf-condition-row"><img
-                              src="{{asset('assets/images/icons/conditions/condition.svg')}}">
-                          <div class="wf-conditions-text">Conditions: clear sky</div>
-                      </div>
-
-                      <div class="wf-condition-row"><img
-                              src="{{asset('assets/images/icons/conditions/humidity.svg ')}}">
-                          <div class="wf-conditions-text">Humidity: 97%</div>
-                      </div>
-                  </div>
-                  <div class="wf-list-col wf-temp-icon">
-                      <div class="wf-icon"><img
-                              src="{{asset('assets/images/icons/weather/01.svg')}}"
-                              class="wf-icon"></div>
-                      <div class="wf-temp">
-                          <div class="wf-temp-max">20°</div>
-                          <div class="wf-temp-min">13°</div>
-                      </div>
-                  </div>
-              </a>
-              <a href="https://meteo.test/location/2964180/day/05" class="wf-list">
-                  <div class="wf-list-col wf-date">
-                      <div class="wf-day">Sunday</div>
-                      <div class="wf-date">08/05/2018</div>
-                  </div>
-                  <div class="wf-list-col wf-conditions">
-                      <div class="wf-condition-row"><img
-                              src="{{asset('assets/images/icons/conditions/condition.svg')}}">
-                          <div class="wf-conditions-text">Conditions: few clouds</div>
-                      </div>
-
-                      <div class="wf-condition-row"><img
-                              src="{{asset('assets/images/icons/conditions/humidity.svg')}}">
-                          <div class="wf-conditions-text">Humidity: 94%</div>
-                      </div>
-                  </div>
-                  <div class="wf-list-col wf-temp-icon">
-                      <div class="wf-icon"><img
-                              src="{{asset('assets/images/icons/weather/02.svg')}}"
-                              class="wf-icon"></div>
-                      <div class="wf-temp">
-                          <div class="wf-temp-max">21°</div>
-                          <div class="wf-temp-min">11°</div>
-                      </div>
-                  </div>
-              </a>
-              <a href="https://meteo.test/location/2964180/day/06" class="wf-list">
-                  <div class="wf-list-col wf-date">
-                      <div class="wf-day">Monday</div>
-                      <div class="wf-date">08/06/2018</div>
-                  </div>
-                  <div class="wf-list-col wf-conditions">
-                      <div class="wf-condition-row"><img
-                              src="{{asset('assets/images/icons/conditions/condition.svg')}}">
-                          <div class="wf-conditions-text">Conditions: light rain</div>
-                      </div>
-
-                      <div class="wf-condition-row"><img
-                              src="{{asset('assets/images/icons/conditions/humidity.svg ')}}">
-                          <div class="wf-conditions-text">Humidity: 82%</div>
-                      </div>
-                  </div>
-                  <div class="wf-list-col wf-temp-icon">
-                      <div class="wf-icon"><img
-                              src="{{asset('assets/images/icons/weather/10.svg')}}"
-                              class="wf-icon"></div>
-                      <div class="wf-temp">
-                          <div class="wf-temp-max">20°</div>
-                          <div class="wf-temp-min">14°</div>
-                      </div>
-                  </div>
-              </a>--}}
         </div>
     </div>
 </div>
